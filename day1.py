89b5942e52b70f12e74ea22aa6182f95b072d5be
Asad <asads@hey.com>
2021-08-12 00:24:57 -0400

    output is not correct

--- day1.py
+++ day1.py
@@ -2,6 +2,12 @@
 
 def unqElement(plist):
 
+    count = {}
+    for i in plist:
+        count.setdefault(i, [1])
+        if(count[i] == [1]):
+            count[i].append(1)
+            print(sum(count[i]))
     return 0
 
 
