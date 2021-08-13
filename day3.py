721ee6f34b66083eabdf7ecfefd6094eccf7c69e
Asad <asads@hey.com>
2021-08-13 18:52:26 -0400

    List element - dict k/v

--- day3.py
+++ day3.py
@@ -4,18 +4,17 @@ def unqElement(plist):
 
     count = {}
     for i in plist:
-        count.setdefault(i, [1])
-        print(count[i])
+        print(count.setdefault(i, 1))
+        print(count.values())
 
-        if(count[1] == 1):
-            pass
+        if(i == count):  # Since i is integer value, using that as dict's index
+            count[i] += 1
 
-    print(count.keys())
-    print(count.values())
+    print(count)
 
     # if(count[i] == [1]):
     # print(sum(count[i]))
     return 0
 
 
-unqElement([0, 0, 1, 0, 1, 2])
+unqElement([0, 0, 5, 0, 8, 8, 8, 5, 5, 8])
