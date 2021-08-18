2088847c1e221f278907b2808260f99d5b1972b4
Asad <asads@hey.com>
2021-08-17 20:04:48 -0400

    Day 4 & 5 work

--- day4.py
+++ day4.py
@@ -4,19 +4,31 @@ def unqElement(plist):
 
     count = {}
     for i in plist:
-        print(i)
+        # print(i)
         # Return key's value if key is present OR create new K/V pair with default value
-        count.setdefault(i, [1])
-        print(count.values())
 
-        if(i == count):  # Since i is integer value, using that as dict's index
-            count.items
+        for c, d in count:
+            if (c == i):
+                d += 1
+            else:
 
-    print(count)
 
-    # if(count[i] == [1]):
-    # print(sum(count[i]))
+        print(count.setdefault(i, [1]))
+        print(count)
+
+        # if (i == count):
+
+        # print(count.values())
+        # if(i == count):  # Since i is integer value, using that as dict's index
+        #            print(count.items)
+
+        # print(count)
+
+        # if(count[i] == [1]):
+        # print(sum(count[i]))
     return 0
 
 
 unqElement([0, 0, 5, 0, 8, 8, 8, 5, 5, 8])
+
+# Incomplete
