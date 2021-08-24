9357a7bca6769b1881062a76fd7a321541108997
Asad <asads@hey.com>
2021-08-23 22:41:05 -0400

    Progress on LC # 9

--- day8.py
+++ day8.py
@@ -1,32 +1,49 @@
 # Palindrome Number (LC # 9)
 
-
 x = 123
 x_s = str(123)
 
 listed = list(x_s)
-print(listed)
+# print(listed)
 
 rev = listed[::-1]
 new_x = ''
-print(new_x.join(rev))
+# print(new_x.join(rev))
 
 
 class Solution(object):
-    def isPalindrome(self, x):
 
+    def __init__(self, x):
         self.x = x
+
+    def isPalindrome(self):
+
         str_x = str(self.x)
 
-        print(str_x)
+        listed = list(str_x)
+
+        rev_list = listed[::-1]
+
+        new_str = ''
+
+        rev_str = new_str.join(rev_list)
+
+        rev_int = int(rev_str)
+
+        if (rev_int == self.x):
+            return True
+        else:
+            return False
+
         """
         :type x: int
         :rtype: bool
         """
+        return 0
 
 
-trial = Solution.isPalindrome
+trial = Solution(929)
 
-print(trial(27))
+print(trial.isPalindrome())
 
 # Reverse Integer (LC # 7 )
