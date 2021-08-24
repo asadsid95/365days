60680c97df3c77449eb884e23db25b70173912b7
Asad <asads@hey.com>
2021-08-23 22:18:52 -0400

    Day 8 work: LC # 9 - Palindrome Number

--- day8.py
+++ day8.py
@@ -1,1 +1,32 @@
-# Reverse Integer
+# Palindrome Number (LC # 9)
+
+
+x = 123
+x_s = str(123)
+
+listed = list(x_s)
+print(listed)
+
+rev = listed[::-1]
+new_x = ''
+print(new_x.join(rev))
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+
+        self.x = x
+        str_x = str(self.x)
+
+        print(str_x)
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+
+trial = Solution.isPalindrome
+
+print(trial(27))
+
+# Reverse Integer (LC # 7 )
