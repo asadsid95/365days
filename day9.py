addca7d6e7687dd67e03f95bc24522390bdcdbdd
Asad <asads@hey.com>
2021-08-24 21:09:39 -0400

    Day 9: Practising for technical interview

--- day9.py
+++ day9.py
@@ -1,1 +1,29 @@
+# Working to practise for 15-min technical assessment
+
+# Calculating length of string
+
+def calc_str_len(input):
+
+    counter = 0
+
+    for i in input:
+        counter += 1
+    return counter
+
+
+# print(calc_str_len('Hello this string\'s length is being counted without built-in function.'))
+
+
+def calc_str_len_2(input):
+    str_list = list(input)
+
+    counter = 0
+    for s in str_list:
+        counter += 1
+    return counter
+
+
+print(calc_str_len_2('hello'))
+
+
 # Reverse Integer (LC # 7 )
