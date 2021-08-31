d4b121533774ae6b5ab512e38293c3cd794d69b4
Asad <asads@hey.com>
2021-08-30 22:34:45 -0400

    Day 12: LC # 7 brute force + updated README

--- day12.py
+++ day12.py
@@ -1,3 +1,22 @@
 # Reverse Integer (LC # 9)
 
-# Input: integer (range of [])
+# Input: integer (range of [-2^31, (2^31)-1])
+# Output: REVERSED integer || 0 if reversed's binary form > 32-bit
+
+# Approach:
+# reverse input, get and compare 32-bit form for input & reversed
+#
+
+def compare(x):
+
+    x_string=str(x)
+    x_rev = int(x_string[::-1])
+
+    if (x_rev == 0):
+        print('Input = 0')
+    elif (x_rev > ((2**31) -1)):
+        print(0)
+    else:
+        print(x_rev, x)
+
+compare(10111)
