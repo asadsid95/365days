77039c9eebb7b587f45b6d849c2c0cff1ea67ed0
Asad <asads@hey.com>
2021-09-01 17:41:45 -0400

    Added class and expense func

--- budget.py
+++ budget.py
@@ -1,1 +1,18 @@
 # Budget app
+
+# Categories: Food, Clothing, Hobby, Entertainment, PC, Coffee
+# Features: expenses i.e. deposit/withdraw funds from each category,
+#              balances/transfer between categories
+
+class Budget():
+    # return 0
+
+
+
+    def exps_calc(self, expense):
+
+        return 0
+    pass
+
+test=Budget()
+print(test)
