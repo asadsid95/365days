52275e050fa66f1f61dd0443e303157546ab760e
Asad <asads@hey.com>
2021-08-31 23:39:20 -0400

    Day 12: LC # 7 now works with +/-ive input + updated README

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ The following will show list of LC problems, files and if it's completed:
 
 1. #1 (Two sum; day6.py) - *passes* with + integers so far
 2. #9 (Palindrome; day7, day8) - *passes* however TypeError on LC regarding  '__init__() takes exactly 2 arguments (1 given)'
+3. #7 (Reverse integer; day12) - *passes* w/ +/-ive inputs; Not tested on LC due to confusion in using classes
--- day12.py
+++ day12.py
@@ -1,4 +1,4 @@
-# Reverse Integer (LC # 9)
+# Reverse Integer (LC # 7)
 
 # Input: integer (range of [-2^31, (2^31)-1])
 # Output: REVERSED integer || 0 if reversed's binary form > 32-bit
@@ -9,14 +9,24 @@
 
 def compare(x):
 
-    x_string=str(x)
-    x_rev = int(x_string[::-1])
+    upper_bound = (2**31) - 1
+    lower_bound = -2**31
+    #print(upper_bound, lower_bound)
+
+    if (x < 0):
+        x_string=str(abs(x))
+        x_rev = int(x_string[::-1])
+        x_rev = x_rev * -1
+    else:
+        x_string=str(x)
+        x_rev = int(x_string[::-1])
 
     if (x_rev == 0):
         print('Input = 0')
-    elif (x_rev > ((2**31) -1)):
+    elif (x_rev > upper_bound):
         print(0)
     else:
         print(x_rev, x)
 
 compare(10111)
+compare(111111110111111111111111111111)
