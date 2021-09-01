f2962d22073efcb6abc709181adcd7c5c069e7ef
Asad <asads@hey.com>
2021-09-01 19:12:54 -0400

    total expense function

--- budget.py
+++ budget.py
@@ -1,18 +1,46 @@
 # Budget app
 
-# Categories: Food, Clothing, Hobby, Entertainment, PC, Coffee
+# Categories: Food, Clothing, Hobby, Subscriptions, PC equip, coffee
+#
 # Features: expenses i.e. deposit/withdraw funds from each category,
 #              balances/transfer between categories
 
-class Budget():
+class Budget:
     # return 0
 
-
+    # dict w/ input value
+    def __init__(self, input):
+        self.input = input     # dict w/ input values
+        return None
 
     def exps_calc(self, expense):
+        # print(expense)
+        # print(self.input.values())
 
-        return 0
-    pass
+        total_expense = 0
 
-test=Budget()
-print(test)
+        for i in self.input.values():
+            total_expense += i
+
+        print(total_expense)
+
+        return None
+
+    # print('end of Budget class')
+
+# print("break")
+
+
+def main():
+
+    default_value=100
+
+    data = {'Food': 300, 'Clothing': 100, 'Hobby': default_value}
+
+    user_input = Budget(data)
+
+    print(user_input.exps_calc(10000))
+
+
+if __name__ == "__main__":
+    main()
