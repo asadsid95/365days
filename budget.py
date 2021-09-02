70e13b764e2c6d292d2603ff5eb3af6db15b3c17
Asad <asads@hey.com>
2021-09-02 00:35:16 -0400

    Comments in budget app

--- budget.py
+++ budget.py
@@ -1,9 +1,22 @@
 # Budget app
 
+# Problem: Create a Budget class that can instantiate ~~~ objects based on different budget categories ~~~ like food, clothing, and entertainment. These objects should allow for depositing and withdrawing funds from each category, as well computing category balances and transferring balance amounts between categories
+
+# IOW:
+
 # Categories: Food, Clothing, Hobby, Subscriptions, PC equip, coffee
 #
 # Features: expenses i.e. deposit/withdraw funds from each category,
 #              balances/transfer between categories
+
+# Input: Dict {} of all expenses i.e. transactions history type data
+# Output: expenses, deposit/wd funds from each catg., balances/transfer between categories
+
+# Topics found during reading on Decorators (Credits: https://book.pythontips.com/en/latest/decorators.html ):
+# - Nested function
+# - function as argument for another func.
+# - ANSWER TO LONG-TIME QUESTION: having () in front of functions executes them (i.e. returns whatever code inside of them) BUT w/o them, function itself is assigned to the variables
+
 
 class Budget:
     # return 0
@@ -13,6 +26,7 @@ class Budget:
         self.input = input     # dict w/ input values
         return None
 
+    # Get total expense
     def exps_calc(self, expense):
         # print(expense)
         # print(self.input.values())
@@ -22,9 +36,10 @@ class Budget:
         for i in self.input.values():
             total_expense += i
 
-        print(total_expense)
+        print('Total expense: ', total_expense)
 
         return None
+
 
     # print('end of Budget class')
 
