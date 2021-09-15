b5ca911c2d333f7fdbe5704bd941160e49439c78
Asad <asads@hey.com>
2021-09-14 22:17:27 -0400

    Described approach for LC # 27 (Remove Element)

--- week5/day13.py
+++ week5/day13.py
@@ -1,1 +1,21 @@
 # Day 13
+# LC # 27 - Remove elements
+
+# Problem statement IMOW:
+# Getting an array/list AND an integer value as input, all occurrences of that value need to be removed from the list
+
+# Input: nums = [1,2,1,1,3,45]
+        # val = 1
+# Output: k=0
+
+# Approach:
+
+# Loop 1:
+# Compare list's first element to val:
+    # - If YES (element matches val), pop()/del() that element + increment k
+    # - If NO, go to next element (via count increment; look into len(nums))
+
+# Loop 2 (assuming first element matched val): nums = [2,1,1,3,45] | val = 1 | k = 1)
+# Compare first element to val:
+    # - If YES (element matches val), pop()/del() that element + increment k
+    # - If NO, go to next element (via count increment; look into len(nums))
